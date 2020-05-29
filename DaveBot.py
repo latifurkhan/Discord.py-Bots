bd0168550c30e65758e36afc13d84cd2289940e9
Latifurk <44888659+latifurkhan@users.noreply.github.com>
2020-05-29 01:37:41 -0400

    Update DaveBot.py
    Added more comments to the code showing basic idea of each command or event

--- DaveBot.py
+++ DaveBot.py
@@ -9,9 +9,10 @@ client = commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
-    print("bot is ready")
+    print("Bot is ready")
 
 
+#anytime someone @ a dave (everyone is dave in the chat) ask which dave
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -25,6 +26,7 @@ async def on_message(message):
     await client.process_commands(message)
 
 
+#Help command explaining the different commands
 @client.command()
 async def h(ctx):
     roll_define = "Type !roll to roll the dice!\n"
@@ -33,11 +35,13 @@ async def h(ctx):
     await ctx.send("Hello daves, this is daveBot:\n" + roll_define + flip_define + dave_function)
 
 
+#Coin flip command !flip
 @client.command()
 async def flip(ctx):
     await ctx.send("Flipped the coin: \n" + random.choice(["Heads", "Tails"]) + " was flipped!")
 
 
+#Dice roll command !roll giving numbers [1-6]
 @client.command()
 async def roll(ctx):
     num = str((random.randrange(6)) + 1)
